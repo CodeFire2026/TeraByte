a40f8f573f8a6442b3ac3d84a753b29bfe5aef4b
Torre Mauro <mauroalexandertorre@gmail.com>
2026-09-08 19:20:41 -0300

    Subo clase 4 de Python

---
+++ Python/Clase4/Funciones.py
@@ -0,0 +1,8 @@
+# Comenzamos con funciones
+# mi_funcion() # No se puede llmar antes de definir una funcion
+# Definimos una función
+def mi_funcion(): # Para identificar a la funcion utilizamos parentesis
+    print("Saludos a todos los alumnos de la Tecnicatura")
+
+mi_funcion() # Estamos usando la funcion
+mi_funcion() # Se puede llamar una funcion N cantidad de veces 
